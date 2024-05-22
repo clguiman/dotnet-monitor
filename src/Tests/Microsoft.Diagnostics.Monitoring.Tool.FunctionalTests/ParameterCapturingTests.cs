2bf2c2eecbea3c6b76749c8de1e3a5fbe77a08a4
Claudiu Guiman <clguiman@microsoft.com>
2024-05-22 13:57:53 -0400

    wip

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/ParameterCapturingTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/ParameterCapturingTests.cs
@@ -78,10 +78,11 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests
 
         [Theory]
         [MemberData(nameof(ProfilerHelper.GetArchitecture), MemberType = typeof(ProfilerHelper))]
-        public async Task NonAspNetAppFailsOperation(Architecture targetArchitecture)
+        public async Task NonStartupHookEnabledAppFailsOperation(Architecture targetArchitecture)
         {
             await RunTestCaseCore(TestAppScenarios.ParameterCapturing.SubScenarios.NonAspNetApp, targetArchitecture, async (appRunner, apiClient) =>
             {
+                // TODO
                 int processId = await appRunner.ProcessIdTask;
 
                 CaptureParametersConfiguration config = GetValidConfiguration();
@@ -95,13 +96,18 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests
 
         [Theory]
         [MemberData(nameof(ProfilerHelper.GetArchitecture), MemberType = typeof(ProfilerHelper))]
+        public Task CapturesParametersInNonAspNetApps(Architecture targetArchitecture) =>
+            CapturesParametersCore(TestAppScenarios.ParameterCapturing.SubScenarios.NonAspNetApp, targetArchitecture, CapturedParameterFormat.JsonSequence);
+
+        [Theory]
+        [MemberData(nameof(ProfilerHelper.GetArchitecture), MemberType = typeof(ProfilerHelper))]
         public Task CapturesParametersAndOutputJsonSequence(Architecture targetArchitecture) =>
-                CapturesParametersCore(targetArchitecture, CapturedParameterFormat.JsonSequence);
+                CapturesParametersCore(TestAppScenarios.ParameterCapturing.SubScenarios.AspNetApp, targetArchitecture, CapturedParameterFormat.JsonSequence);
 
         [Theory]
         [MemberData(nameof(ProfilerHelper.GetArchitecture), MemberType = typeof(ProfilerHelper))]
         public Task CapturesParametersAndOutputNewlineDelimitedJson(Architecture targetArchitecture) =>
-                CapturesParametersCore(targetArchitecture, CapturedParameterFormat.NewlineDelimitedJson);
+                CapturesParametersCore(TestAppScenarios.ParameterCapturing.SubScenarios.AspNetApp, targetArchitecture, CapturedParameterFormat.NewlineDelimitedJson);
 
 #else // NET7_0_OR_GREATER
         [Theory]
@@ -122,9 +128,9 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests
         }
 #endif // NET7_0_OR_GREATER
 
-        private async Task CapturesParametersCore(Architecture targetArchitecture, CapturedParameterFormat format)
+        private async Task CapturesParametersCore(string subScenarioName,Architecture targetArchitecture, CapturedParameterFormat format)
         {
-            await RunTestCaseCore(TestAppScenarios.ParameterCapturing.SubScenarios.AspNetApp, targetArchitecture, async (appRunner, apiClient) =>
+            await RunTestCaseCore(subScenarioName, targetArchitecture, async (appRunner, apiClient) =>
             {
                 int processId = await appRunner.ProcessIdTask;
 
